9147ece523206770a15b862cf1b929a544997dbb
by <beyonne@126.com>
2019-08-06 17:52:25 +0800

    Music player V1.0.2

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # MyMusicPalyer
 my first andriod app designed by myself,a singer native music player
+V1.0.2   20190806
+１增加ListView单个项目点击事件，并取得该项目的绝对路径；
+
+
 V1.0.1   20190802
 １工程编译成功；
 ２仅实现扫描手机本地音频文件并添加到ListView中．
--- app/src/main/java/com/example/mymusic/MainActivity.java
+++ app/src/main/java/com/example/mymusic/MainActivity.java
@@ -3,8 +3,11 @@ package com.example.mymusic;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.List;
 
@@ -25,5 +28,14 @@ public class MainActivity extends AppCompatActivity {
         musicInfos = MusicUtil.getMp3Infos(MainActivity.this);
         Listadapter = new MusicListAdapter(this, musicInfos);
         listview.setAdapter(Listadapter);
+
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String tr = musicInfos.get(i).getUrl();//获取绝对路径
+                Toast.makeText(MainActivity.this,"Path:"+ tr,Toast.LENGTH_SHORT).show();
+            }
+        });
     }
+//    listview.setOnItemClickLi
 }
